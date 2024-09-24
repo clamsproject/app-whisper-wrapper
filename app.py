812b6552e9fef21e5837ea0da04f2ef68bf944b6
Keigh Rim <keigh.rim@gmail.com>
2024-09-24 09:47:04 -0400

    copied default decoder configs for whisper transcriber model, added some logging msgs for profiling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import re
+import time
 from typing import Union
 
 import whisper
@@ -47,29 +48,52 @@ class WhisperWrapper(ClamsApp):
         if lang == 'en' and not size.startswith('large'):
             size += '.en'
         self.logger.debug(f'whisper model: {size} ({lang})')
-        transcribe_args = {}
+        # taken from the default values for decoder arguments in whisper cli
+        transcribe_args = {'best_of': 5, 
+                           'beam_size': 5,
+                           "patience": None,
+                           "length_penalty": None,
+                           "suppress_tokens": "-1",
+                           }
         for param in self.metadata.parameters:
             if param.description.startswith(app_metadata.whisper_argument_delegation_prefix):
                 pattern = re.compile(r'(?<!^)(?=[A-Z])')
                 transcribe_args[pattern.sub('_', param.name).lower()] = parameters[param.name]
-        self.logger.debug(f'whisper model args: {transcribe_args}')
-
+        # this is due to the limitation of the SDK that doesn't allow `None` for a default value for a parameter
+        # (setting default to None is a reserved action to make the parameter optional)
+        # So as a workaround, the default is set to an empty string, then to match the behavior of the whisper cli,
+        # it's converted to None here.
+        if transcribe_args['initial_prompt'] == '':
+            transcribe_args['initial_prompt'] = None
         if size not in self.whisper_models:
-            self.logger.debug(f'model not cached, downloading now')
+            self.logger.debug(f'Loading model {size}')
+            t = time.perf_counter()
             self.whisper_models[size] = whisper.load_model(size)
+            self.logger.debug(f'Load time: {time.perf_counter() - t:.2f} seconds\n')
             self.model_usage[size] = False
-        if self.model_usage[size] == False:
+        if not self.model_usage[size]:
             whisper_model = self.whisper_models.get(size)
             self.model_usage[size] = True
             cached = True
         else:
+            self.logger.debug(f'Loading model {size} to avoid memory conflict')
+            t = time.perf_counter()
             whisper_model = whisper.load_model(size)
+            self.logger.debug(f'Load time: {time.perf_counter() - t:.2f} seconds\n')
             cached = False
 
         for doc in docs:
-            transcript = whisper_model.transcribe(audio=doc.location_path(nonexist_ok=False), word_timestamps=True, 
-                                                  language=lang if len(lang) > 0 else None, **transcribe_args)
+            transcribe_args['language'] = lang if len(lang) > 0 else None
+            transcribe_args['word_timestamps'] = True
+            self.logger.debug(f'whisper model args: {transcribe_args}')
+            self.logger.debug('Transcribing audio')
+            t = time.perf_counter()
+            transcript = whisper_model.transcribe(audio=doc.location_path(nonexist_ok=False), 
+                                                  **transcribe_args)
+            self.logger.debug(f'Transcription time: {time.perf_counter() - t:.2f} seconds\n')
             # keep the original language parameter, that might have region code as well
+            self.logger.debug(f'Preparing a new transcript view for {doc.id}')
+            t = time.perf_counter()
             lang_to_record = parameters['modelLang'] if len(parameters['modelLang']) > 0 else transcript['language']
             view: View = mmif.new_view()
             self.sign_view(view, parameters)
@@ -77,7 +101,11 @@ class WhisperWrapper(ClamsApp):
             view.new_contain(Uri.TOKEN)
             view.new_contain(AnnotationTypes.TimeFrame, timeUnit=app_metadata.timeunit, document=doc.id)
             view.new_contain(AnnotationTypes.Alignment)
+            self.logger.debug(f'View preparation time: {time.perf_counter() - t:.2f} seconds\n')
+            self.logger.debug(f'Translating into MMIF')
+            t = time.perf_counter()
             self._whisper_to_textdocument(transcript, view, mmif.get_document_by_id(doc.id), lang=lang_to_record)
+            self.logger.debug(f'Translation time: {time.perf_counter() - t:.2f} seconds\n')
         
         if size in self.model_usage and cached == True:
                 self.model_usage[size] = False
@@ -100,7 +128,7 @@ class WhisperWrapper(ClamsApp):
                 tok_start = raw_text.index(raw_token, char_offset)
                 tok_end = tok_start + len(raw_token)
                 char_offset = tok_end
-                token = view.new_annotation(Uri.TOKEN, word=raw_token, start=tok_start, end=tok_end, document=f"{view.id}:{textdoc.id}")
+                token = view.new_annotation(Uri.TOKEN, word=raw_token, start=tok_start, end=tok_end, document=f'{view.id}:{textdoc.id}')
                 token_ids.append(token.id)
                 tf_start = int(word["start"] * 1000)
                 tf_end = int(word["end"] * 1000)
